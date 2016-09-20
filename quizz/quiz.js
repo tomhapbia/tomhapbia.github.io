a47cb48ec35654b2b39d18d4906995dfc29ea84b
lewisflynz <sieugadhv@gmail.com>
2016-09-20 10:51:04 +0700

    LOL quizzz

--- quizz/quiz.js
+++ quizz/quiz.js
@@ -81,19 +81,49 @@ var question10 = {
 
 var arrQuestion = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
 
-for (var i = 1; i <= 6; i++) {
-	var idcauhoi = "#name" + i;
-	var cauhoi = document.querySelector(idcauhoi);
-	cauhoi.innerHTML = arrQuestion[i-1].question;
-	for (var j = 1; j <= 3; j++) {
-		var iddapan = "#dapan" + i + j;
-		var ans = document.querySelector(iddapan);
-		var chiso = "ans";
-		chiso += j;
-		ans.innerHTML = arrQuestion[i-1][chiso];
-	};
-};
+for (var i = arrQuestion.length - 1; i > 0; i--) {
+	var j = Math.floor(Math.random() * (i + 1));
+	var temp = arrQuestion[i];
+	arrQuestion[i] = arrQuestion[j];
+	arrQuestion[j] = temp;
+}
+var socauhoi = 0;
+var idques = "cau";
+function checknumques() {
+	var input = document.getElementById("socauhoi");
+	var socauhoi = input.value;
+	// alert(isNaN(socauhoi));
+	if (isNaN(socauhoi)) {
+		alert("Nhap vao \"so\" cau hoi =.=");
+		location.href = "index.html";
 
+	} else{
+		for (var i = 1; i <= socauhoi; i++) {
+			document.getElementById(idques + i).style.display = "block";
+
+		};
+		document.getElementById("check").style.display = "block";
+
+		var danhsachdapan = [];
+
+		for (var i = 0; i <= socauhoi; i++) {
+			danhsachdapan.push(arrQuestion[i].ans);
+		};
+
+		for (var i = 1; i <= socauhoi; i++) {
+			var idcauhoi = "#name" + i;
+			var cauhoi = document.querySelector(idcauhoi);
+			cauhoi.innerHTML = arrQuestion[i-1].question;
+			for (var j = 1; j <= 3; j++) {
+				var iddapan = "#dapan" + i + j;
+				var ans = document.querySelector(iddapan);
+				var chiso = "ans";
+				chiso += j;
+				ans.innerHTML = arrQuestion[i-1][chiso];
+			}
+		}
+	};
+}
 
 function changeColor(dau) {
 	id = dau.id;
@@ -101,13 +131,13 @@ function changeColor(dau) {
 	for (var i = 1; i <= 3; i++) {
 		if (dau.id.slice(-1) != i) {
 			if (document.getElementById(id+i).getAttribute("class") == "abc") {
-				document.getElementById(id+i).setAttribute("class", "dapan")
+				document.getElementById(id+i).setAttribute("class", "dapan");
 			}
 		} else {
 			dau.setAttribute("class", "abc");
 		}
 	}
-};
+}
 
 var traloiall = [];
 var ansall = [];
@@ -116,43 +146,19 @@ var b = [];
 var caudung = 0;
 var flag = false;
 function check() {
+	var input1 = document.getElementById("socauhoi");
+	var socauhoi1 = input1.value;
 	traloiall = document.querySelectorAll(".abc");
 	for (var i = 0; i < traloiall.length; i++) {
 		if (traloiall[i].innerHTML == arrQuestion[i].ans) {
 			caudung++;
 		}
-	};
-	if (caudung == 6) {
-		alert("Chuc mung ban!");
-		location.href = "index.html"
+	}
+	if (caudung == socauhoi1) {
+		alert("Chuc mung ban da tra loi dung toan bo cau hoi");
+		location.href = "index.html";
 	} else{
-		alert("Chia buon voi ban :'(. Ban da tra loi dung " + caudung);
-	};
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		alert("Chia buon voi ban :'(. Ban da tra loi dung " + caudung + " cau!");
+			location.href = "index.html";
+		}
+	}
