4606f1f0a39d50b0e679c74cb28bce7a8f0e6136
lewisflynz <sieugadhv@gmail.com>
2016-09-30 15:04:10 +0700

    bs3-fix list product

--- tomstorebs3/js/script.js
+++ tomstorebs3/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+	// wow js 
+
 	new WOW().init();
+
+
+	// show/hide mobile navbar
 
 	$('.navbar-toggle').on('click', function(){
 		$('li.dropdown').children('ul').removeClass('dropdown-show');
@@ -15,11 +20,53 @@ $(document).ready(function() {
 		};
 	});
 
+	//control carousel
 	
 	$("#myBtn").click(function(){
-        $("#bs-carousel").carousel("prev");
-    });
-    $("#myBtn2").click(function(){
-        $("#bs-carousel").carousel("next");
-    });
+		$("#bs-carousel").carousel("prev");
+	});
+	$("#myBtn2").click(function(){
+		$("#bs-carousel").carousel("next");
+	});
+
+
+	// config datepicker
+
+	$( function() {
+		$( "#datepicker" ).datepicker({
+			changeMonth: true,
+			changeYear: true,
+			showAnim: "fold",
+			dateFormat: "dd-mm-yy"
+		});
+	} );
+
+	// config back to top
+
+	var offset = 300;
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > offset) {
+			$('#btn-to-top').css('display', 'block');
+		} else{
+			$('#btn-to-top').css('display', 'none');
+		};
+	});
+
+	$('#btn-to-top').click(function(){
+		$('html,body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+
 });
+
+
+
+
+
+
+
+
