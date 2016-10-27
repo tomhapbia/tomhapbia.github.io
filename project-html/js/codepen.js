85c8c42ed5ddae71b9e24e05a188e105971c5813
lewisflynz <sieugadhv@gmail.com>
2016-10-27 17:15:02 +0700

    add leaderboard, challenge

--- project-html/js/codepen.js
+++ project-html/js/codepen.js
@@ -1,16 +1,21 @@
 /**
+ * Created by Tom on 10/27/16.
+ */
+
+
+/**
  * Created by Tom on 10/7/16.
  */
 
-$(document).ready(function () {
+$(document).ready(function(){
 
-    $(".navbar-default .navbar-nav>li").hover(function () {
+    $(".navbar-default .navbar-nav>li").hover(function(){
         $(".bottom-line").css("border", "none");
         $(".navbar-brand").css("border-top", "1px solid #33485F");
-        $(this).css("border-top", "1px solid #ccc");
-    }, function () {
+        $(this).css("border-top","1px solid #ccc");
+    }, function(){
         $(".bottom-line").css("border-top", "1px solid #ccc");
-        $(this).css("border-top", "none");
+        $(this).css("border-top","none");
         $(".navbar-brand").css("border-top", "none");
     });
 
@@ -20,107 +25,91 @@ $(document).ready(function () {
     Waves.attach('.signup-banner');
     Waves.init();
 
-// WOW 
+// WOW
 
     new WOW().init();
 
-// Display KhoaHocTag 
+// Display KhoaHocTag
 
-    if ($('.tagKhoahoc').css('margin-bottom') == '0px') {
+    if ($('.tagKhoahoc').css('margin-bottom') == '0px'){
         var positionKhoaHocTag = $('.khoahoctag').offset();
-        $(window).scroll(function () {
+        $(window).scroll(function(){
             if ($(this).scrollTop() >= positionKhoaHocTag.top) {
                 $('.khoahoctag').addClass('khoahocPositionFixed');
-            } else {
+            }else {
                 $('.khoahoctag').removeClass('khoahocPositionFixed');
             }
         });
     }
 
 
-//COUNTER
-
-
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
 
 
 // Back to top
 
 
-    $(window).scroll(function () {
+    $(window).scroll(function(){
         if ($(this).scrollTop() > 200) {
             $('#btn-to-top').css('display', 'block');
-        } else {
+        } else{
             $('#btn-to-top').css('display', 'none');
-        }
-        ;
+        };
     });
-    $('#btn-to-top').click(function () {
+    $('#btn-to-top').click(function(){
         $('html,body').animate({
             scrollTop: 0
         }, 1000);
     });
 
-// Show modal dang nhap/dangki 
+// Show modal dang nhap/dangki
 
-    $('#btn-dangki').on('click', function () {
+    $('#btn-dangki').on('click', function(){
         $('.bs-example-modal-lg').modal('show');
         $('#myTabs a[href="#profile"]').tab('show');
     })
 
-    $('#btn-dangki2').on('click', function () {
-        $('.bs-example-modal-lg').modal('show');
-        $('#myTabs a[href="#profile"]').tab('show');
-    })
-
-    $('#btn-dangnhap').on('click', function () {
+    $('#btn-dangnhap').on('click', function(){
         $('.bs-example-modal-lg').modal('show');
         $('#myTabs a[href="#home"]').tab('show');
     })
 
 // fix height index
     var maxHeight = 0;
-    $('.content-description').each(function () {
-        if ($(this).height() > maxHeight) {
+    $('.content-description').each(function(){
+        if ($(this).height() > maxHeight){
             maxHeight = $(this).height();
         }
     });
 
-    $('.content-description').each(function () {
+    $('.content-description').each(function(){
         $(this).height(maxHeight);
     })
 
-// fix height codepen 
+// fix height codepen
 
     var maxHeightCodepen = 0;
-    $('.pen-detail').each(function () {
-        if ($(this).height() > maxHeightCodepen) {
+    $('.pen-description h3').each(function(){
+        if ($(this).height() > maxHeightCodepen){
             maxHeightCodepen = $(this).height();
         }
     });
-    $('.pen-detail').each(function () {
+    $('.pen-description h3').each(function(){
         $(this).height(maxHeightCodepen);
     })
 
 
-// fix height khoahocList 
+// fix height khoahocList
 
     var maxHeightKhoaHocList = 0;
-    $('.khoahoc-detail').each(function () {
-        if ($(this).height() > maxHeightKhoaHocList) {
+    $('.khoahoc-detail').each(function(){
+        if ($(this).height() > maxHeightKhoaHocList){
             maxHeightKhoaHocList = $(this).height();
         }
     });
-    $('.khoahoc-detail').each(function () {
+    $('.khoahoc-detail').each(function(){
         $(this).height(maxHeightKhoaHocList);
     })
+
 
 
 });
